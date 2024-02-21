68db9ad43e0e5789622c646c99ac476ad961f8f0
TheSoftNode <thesoftnode@gmail.com>
2024-02-21 15:20:06 +0100

    Add user account service and it's contract

---
+++ ClientLibrary/Services/Contracts/IUserAccountService.cs
@@ -0,0 +1,10 @@
+﻿using BaseLibrary.DTOs;
+
+namespace ClientLibrary.Services.Contracts;
+
+public interface IUserAccountService
+{
+	Task<GeneralResponse> CreateAsync(Register user);
+	Task<LoginResponse> SignInAsync(Login user);
+	Task<LoginResponse> RefreshTokenAsync(RefreshToken token);
+}
