69bfe32d3d20f44503ce3b8a3c507812efeab7c5
TheSoftNode <thesoftnode@gmail.com>
2024-02-21 15:17:39 +0100

    Add Serializations and local storage helpers
    Added a class to handle Serializations to json and otherwise
    Added a class to handle local storage of tokens and refresh tokens

---
+++ Client/Helpers/LocalStorageService.cs
@@ -0,0 +1,11 @@
+﻿using Blazored.LocalStorage;
+
+namespace Client.Helpers;
+
+public class LocalStorageService(ILocalStorageService localStorageService)
+{
+	private const string StorageKey = "authentication-token";
+	public async Task<string> GetToken() => await localStorageService.GetItemAsStringAsync(StorageKey);
+	public async Task setToken(string item) => await localStorageService.SetItemAsStringAsync(StorageKey, item);
+	public async Task RemoveToken() => await localStorageService.RemoveItemAsync(StorageKey);
+}
